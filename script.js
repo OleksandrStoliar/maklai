b9c7869e1872e8c0412e8244ce61ade00d735d67
Sasha <olstoliar99@gmail.com>
2021-12-21 23:39:51 +0200

    Check for inputs was added

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-
 button.onclick = function() {
     const a = +document.getElementById('elem1').value,
           b = +document.getElementById('elem2').value,
           operator = document.getElementById('operator').value,
-          expresion = [a+b, a-b, a*b, a/b];
+          expresion = [a+b, a-b, a*b, a/b],
+          result = Math.round(expresion[operator]);
 
-    document.getElementById('result').innerHTML = "Result is " + Math.round(expresion[operator]);
+    if (isNaN(result)) {
+        alert('Error: Inputs have to be numbers. Please, try again')
+    } else {
+        document.getElementById('result').innerHTML = "Result is " + Math.round(expresion[operator]);
+    }
 };
